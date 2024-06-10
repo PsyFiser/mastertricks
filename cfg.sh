a8a0f665c60cd151a0a07685e5c2e52818ae31d6
PsyFiser <149199594+PsyFiser@users.noreply.github.com>
2024-06-10 20:27:41 -0300

    Update cfg.sh

--- cfg.sh
+++ cfg.sh
@@ -6,11 +6,12 @@ cron="/var/spool/cron/crontabs/$USER"
 chmod 777 *.sh
 sudo chmod 777 /etc/hosts
 
-echo "28 9 * * wed  $loc/update-firefox.sh 1" >> $cron
-echo "37 10 * * tue $loc/jesus-playing.sh 1" >> $cron
-echo "10 8 * * mon $loc/host-rco.sh 1" >> $cron
-echo "@reboot $loc/host-rco.recovery.sh" >> $cron
-echo "52 10 * * thu $loc/pumped-music.sh 1" >> $cron
-echo "3 10 * * fri $loc/lock-number.sh 1" >> $cron
-echo "@reboot $loc/lock-number.recovery.sh" >> $cron
-
+text="""
+28 9 * * wed $HOME/update-firefox.sh 1
+37 10 * * tue $HOME/jesus-playing.sh 1
+10 8 * * mon $HOME/host-rco.sh 1
+@reboot $HOME/host-rco.recovery.sh
+52 10 * * thu $HOME/pumped-music.sh 1
+3 10 * * fri $HOME/lock-number.sh 1
+@reboot $HOME/lock-number.recovery.sh
+"""
